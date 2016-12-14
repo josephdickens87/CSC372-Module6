7ce3875bb7bff627bacf1ff24d602992a7b3869f
josephdickens87 <josephdickens87@gmail.com>
2016-12-14 17:04:16 -0500

    Add files via upload
    updated code, to include user input validation, etc.

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,5 +1,19 @@
+/*
+Joseph Dickens
+Programming II
+
+This program impliments 2 methods, both of which are selction sort methods.
+One method will assort userinput, in alphabetical (ascending order)
+The other method will sort in reverse alphabetical (desscending order)
+
+This program has only one class, the main method simply tests 
+functionality of the sorting methods
+*/
+
+// import java utilities
 import java.util.*;
 
+// create class
 public class SelectionSort
 {
 
@@ -16,47 +30,96 @@ public class SelectionSort
         // prompt user for name input
         do
         {
-            System.out.println("Please input a name, and press enter.");
+            System.out.println("Please input a name, and press enter.");                    
             String input = in.nextLine();
-            namesArrayList.add(input);
+            
+            // user input validation to accepts letters only
+            if (!input.matches("\\p{Alpha}*"))
+            
+            {
+            System.out.println("Please input a name only, try again.\n");
+            }
+            
+            // if input is letters only, add to array list
+            else namesArrayList.add(input);
         }
-
+        
         // assignment said ten names, so this is to limit input amount
         while(namesArrayList.size()<10);
 
-        // print the original array list
-        System.out.println(namesArrayList);
+        // print the original array list as inputed for 
+        // debugging and comparison
+        System.out.println("You entered:\n" +namesArrayList +"\n");
 
-        //convert ArrayList to an Array, then initialize it
-        String[] namesArray = new String[namesArrayList.size()];
-        namesArray = namesArrayList.toArray(namesArray);
-
-        // selection sort it
-        selectionSorter(namesArray);
-
-        // print the array out after being sorted
-        System.out.println(Arrays.toString(namesArray));
+        // selection sort alphabetical order (ascending
+        selectionSortAscend(namesArrayList);
+        
+        // print results after ascending order sort
+        System.out.println("After sorting alphabetical order :\n");
+        namesArrayList.forEach(s -> System.out.println(s));
+        
+        
+        // sort in reverese alphabetical order (desscending)
+        selectionSortDesscend(namesArrayList);
+        
+        // print results after desscending order sort
+        System.out.println("\nAfter sorting reverse alphabetical order :\n");
+        namesArrayList.forEach(s -> System.out.println(s));
+                
     }
 
-
-    public static void selectionSorter(String[] array)
+    // method to use selection sort for alphabeticla order
+    public static void selectionSortAscend(ArrayList<String> array)
     {
-        for (int i = 0; i < array.length - 1; ++i)
+        // for loop to loop through array elements
+        for (int i = 0; i < array.size() - 1; i++)
         {
+            // assume first element is minimum
             int minIndex = i;
-
-            for (int j = i + 1; j < array.length; ++j)
+            
+            // for loop to loop through elements that have not been sorted
+            for (int j = i + 1; j < array.size(); j++)
             {
-
-                if (array[j].compareTo(array[minIndex]) < 0)
+                // if statement to compare elements for swap.
+                if (array.get(j).compareTo(array.get(minIndex)) < 0)
                 {
                     minIndex = j;
                 }
             }
+            
+            // variables for method.
+            String temp = array.get(i);
+            array.set(i,array.get(minIndex));
+            array.set(minIndex,temp);
+        }
+    }
 
-            String temp = array[i];
-            array[i] = array[minIndex];
-            array[minIndex] = temp;
+   /* 
+   method to use selection sort for reverse alphabeticla order
+   Everything is the same for this method as the ascending order,
+   except that I have replaced "min" with "max" and replaced "<"
+   with ">".
+   */   
+   public static void selectionSortDesscend(ArrayList<String> array)
+    {
+        for (int i = 0; i < array.size() - 1; i++)
+        {
+            int maxIndex = i;
+
+            for (int j = i + 1; j < array.size(); j++)
+            {
+                // switched "<" with ">" in this line.
+                if (array.get(j).compareTo(array.get(maxIndex)) > 0)
+                {
+                    maxIndex = j;
+                }
+            }
+
+            String temp = array.get(i);
+            array.set(i,array.get(maxIndex));
+            array.set(maxIndex,temp);
         }
     }
 }
+
+   
